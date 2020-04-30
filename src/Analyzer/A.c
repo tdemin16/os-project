ba0e9b178e2b117961cae6461ca23eb06f934f8d
Thomas <thomasdemin16@gmail.com>
2020-04-30 17:37:42 +0200

    Init A.c lib.h lib.c

---
+++ src/Analyzer/A.c
@@ -0,0 +1,10 @@
+#include "../lib/lib.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char const *argv[]) {
+
+    
+    
+    return 0;
+}
---
+++ src/lib/lib.h
@@ -0,0 +1,4 @@
+#ifdef __LIB_H__
+#define __LIB_H__
+
+#endif
