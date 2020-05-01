43c80be94b701dc3c2b62bbe87635362fc85e4bc
Thomas <thomasdemin16@gmail.com>
2020-05-01 14:14:01 +0200

    Move lib declaration to lib.c

--- src/lib/lib.c
+++ src/lib/lib.c
@@ -1,1 +1,41 @@
-#include "../lib/lib.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include "lib.h"
+
+node init_list(char* p) {
+    node tmp = (node)malloc(sizeof(List));
+    tmp->path = p;
+    tmp->next = NULL;
+
+    return tmp;
+}
+
+node insert_first(char* p, node l) {
+    //if l is null, it will produce the same result as initList
+    //so it doesn't require a test
+    node tmp;
+    tmp->path = p;
+    tmp->next = l;
+
+    return tmp;
+}
+
+//Boolean result
+char is_present(char* p, node l) {
+    int ret = FALSE;
+    node tmp = l;
+
+    if(tmp == NULL) {
+        printf("Lista vuota\n");
+    }
+    else {
+        while(tmp != NULL && !ret) {
+            if(!strcmp(p, tmp->path)) {
+                ret = TRUE;
+            }
+            tmp = tmp->next;
+        }        
+    }
+
+    return ret;
+}
--- src/lib/lib.h
+++ src/lib/lib.h
@@ -10,49 +10,15 @@
 #define TRUE 1
 #define FALSE 0
 
-typedef struct List{
-    
+struct List{
     char* path;
-    List* next;
-
+    struct List* next;
 }List;
 
-List* init_list(char* p) {
-    List* tmp = (List*)malloc(sizeof(List));
-    tmp->path = p;
-    tmp->next = NULL;
+typedef struct List* node;
 
-    return tmp;
-}
-
-List* insert_first(char* p, List* l) {
-    //if l is null, it will produce the same result as initList
-    //so it doesn't require a test
-    List* tmp;
-    tmp->path = p;
-    tmp->next = l;
-
-    return tmp;
-}
-
-//Boolean result
-int is_present(char* p, List* l) {
-    int ret = FALSE;
-    List* tmp = l;
-
-    if(tmp == NULL) {
-        printf("Lista vuota\n");
-    }
-    else {
-        while(tmp != NULL && !ret) {
-            if(!strcmp(p, tmp->path)) {
-                ret = TRUE;
-            }
-            tmp = tmp->next;
-        }        
-    }
-
-    return ret;
-}
+node init_list(char* p);
+node insert_first(char* p, node l);
+char is_present(char* p, node l);
 
 #endif
