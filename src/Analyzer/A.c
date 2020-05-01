ddc6881853e096ce711cb267953f009a51297f47
Thomas <thomasdemin16@gmail.com>
2020-05-01 13:48:53 +0200

    Add List structure (with functions). Add .gitignore file

--- src/Analyzer/A.c
+++ src/Analyzer/A.c
@@ -1,8 +1,11 @@
 #include "../lib/lib.h"
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/wait.h>
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char *argv[]) {
 
     
     
