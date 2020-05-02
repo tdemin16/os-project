964cdb08ea5445575a542c317860c6cd3eb7aaa9
Luigi Pusiol <luigi.pusiol@studenti.unitn.it>
2020-05-02 17:29:02 +0200

    New parser of A, list still not working

--- src/Analyzer/A.c
+++ src/Analyzer/A.c
@@ -2,19 +2,23 @@
 
 int main(int argc, char *argv[])
 {
+    //Parsing arguments------------------------------------------------------------------------------------------
     int n = 3;
     int m = 4;
-    int i;
+    int i; //Variabile usata per ciclare gli argomenti (argv[i])
     int value_return = 0; //Valore di ritorno
     int count = 0; //numero di file univoci da analizzare
     node filePath = NULL; //list of path's strings
+    char *tmp;
     //ATTENZIONE: args puo' essere sostituita da filePath qualora questa non sia piu' utile dopo il fork
     //Rimuovere questi commenti alla fine del progetto :)
     node msg; //list used to pass path's to child
 
     char flag = FALSE; // se flag = true, l'argomento successivo è il numero o di n o di m
-    char setn = FALSE; // quando 
-    char setm = FALSE;
+    char setn = FALSE; // se setn = true, n è stato cambiato
+    char setm = FALSE; // se setn = true, m è stato cambiato
+    FILE *fp;
+    char riga[1035];
 
     //Variables for IPC
     int fd_1[2]; //Pipe
@@ -22,79 +26,68 @@ int main(int argc, char *argv[])
     pid_t f; //fork return value
     char args[8][7];
 
-
-    if (argc > 1) //APERTURA TRAMITE MAIN (ARGOMENTI)
-    {
-        system("clear");
-        for (i = 1; i < argc; i++) //ciclo che controlla ogni argomento
-        {
-            if (!strcmp("-setn", argv[i]))
-            { //Controllo se si vuole cambiare n
-                n = atoi(argv[i + 1]); //presuppongo che l'argomento dopo sia un numero, bisogna aggiungere un controllo
-                flag = TRUE;           //il prossimo argomento è il valore di n o m, non va analizzato
+    if(argc < 1) { //if number of arguments is even or less than 1, surely it's a wrong input
+        value_return = err_args_A();
+    }
+    else {
+        for(i = 1; i < argc && value_return == 0; i++) {
+            if(!strcmp(argv[i], "-setn")) {//Check if argument is equal to -setn
+                n = atoi(argv[i + 1]);
+                if(n == 0) value_return = err_args_A();
+                flag = TRUE;
                 i++;
             }
-            else
-            {
-                if (!strcmp("-setm", argv[i]))
-                {                          //Controllo se si vuole cambiare m
-                    m = atoi(argv[i + 1]); //presuppongo che l'argomento dopo sia un numero, bisogna aggiungere un controllo
-                    flag = TRUE;           //il prossimo argomento è il valore di n o m, non va analizzato
-                    i++;
-                }
+            else if(!strcmp(argv[i], "-setm")) { //Check if argument is equal to -setm
+                m = atoi(argv[i+1]);
+                if(m == 0) value_return = err_args_A();
+                flag = TRUE;
+                i++;
+            }else if(strncmp(argv[i], "-", 1) == 0){ //Se inizia per - ma non è setn/setm non è un input valido
+                value_return = err_args_A();
             }
 
-            if (flag == FALSE) //se è falso vuol dire che il prossimo argomento è un file o una cartella
-            {
-                printf("\n ANALIZZO: %s\n", argv[i]);
-                char *analyze = argv[i]; //Costruzione comando
-                int dim = strlen("find ") + strlen(analyze) + strlen(" -type f -follow -print" + 1);
-                char command[dim];
+            if (flag == FALSE){ //Vuol dire che argv[i] e' un file o una cartella
+                char command[strlen("find ") + strlen(argv[i]) + strlen(" -type f -follow -print" + 1)];
                 strcpy(command, "find ");
-                strcat(command, analyze);
+                strcat(command, argv[i]);
                 strcat(command, " -type f -follow -print");
-
-                FILE *fp;
-                char riga[1035];
-
                 fp = popen(command, "r"); //avvia il comando e in fp prende l'output
-                if (fp == NULL)
+                if (fp == NULL) 
                 {
-                    printf("Failed to run command\n");
-                    exit(1);
-                }
-                
-                while (fgets(riga, sizeof(riga), fp) != NULL) //Legge riga per riga e aggiunge al set
+                    value_return = err_args_A();
+                } else { //Il comando va a buon fine
+                    while (fgets(riga, sizeof(riga), fp) != NULL) //Legge riga per riga e aggiunge alla lista
                 {
                     char resolved_path[PATH_MAX];
-                    realpath(riga, resolved_path);
+                    realpath(riga, resolved_path);  //risalgo al percorso assoluto
+                    resolved_path[strlen(resolved_path)-1] = 0; //tolgo l'ultimo carattere che manderebbe a capo                     
                     
                     
                     if (!(is_present(resolved_path, filePath))){
-                        count = count + 1;
                         filePath = insert_first(resolved_path,filePath);
-                        printf("\nNuovo inserimento\n");
+                        printf("[+] %s\n",resolved_path);
+                        count++;
                     } else {
-                        printf("- E' gia' presente");
+                        printf("[-] %s\n",resolved_path);
                     }
-                    printf("%d - %s", count, resolved_path);
+                    
+
                 }
                 pclose(fp);
+                }
+            } else {
+                flag = FALSE;
             }
-            flag = FALSE;
+
+
         }
-        //printf("\nFile da analizzare: %lu\nda dividere in n=%d e m=%d\n", set_length(&filePath),n,m);
-        printf("\nn=%d e m=%d\n", n, m);
+        if(count == 0 && value_return == 0) value_return = err_args_A(); //counter is higher than zero, if not gives an error (value_return used to avoid double messages)
     }
-    else //APERTURA MANUALE (SENZA ARGOMENTI)
-    {
-        value_return = err_args_A(); //in caso di errore setta il valore di ritorno a ERR_ARGS
-    }
-
-    if(value_return == 0 && count == 0) {
-        value_return = err_file();
-    }
-
+    
+    printf("\nNumero file: %d,n=%d m=%d\n",count,n,m);
+    
+ 
+/*
     //IPC
     if(value_return == 0) { //Testo che non si siano verificati errori in precedenza
         if(pipe(fd_1) == -1) { //Controllo se nella creazione della pipe ci sono errori
@@ -143,6 +136,6 @@ int main(int argc, char *argv[])
             execvp(args[0], args);
         }
     } 
-
+    */
     return value_return;
 }
