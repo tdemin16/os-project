38c1fc7b571c8c6253d2583bb5ede6728991e601
Luigi Pusiol <luigi.pusiol@studenti.unitn.it>
2020-05-06 12:26:54 +0200

    Added printInfoCluster function

--- src/lib/lib.c
+++ src/lib/lib.c
@@ -223,6 +223,43 @@ void printStat_Cluster(char * char_count){
     printf("\n");
 }
 
+void printInfoCluster(){
+    int v[DIM_V];
+    int i;
+    printf("Lettere minuscole:\t");
+    for (i = 65; i<=90; i++){
+        printf("%c ",i+32);
+    }
+    printf("\n");
+    printf("Lettere maiuscole:\t");
+    for (i = 33; i<=58; i++){
+        printf("%c ",i+32);
+    }
+    printf("\n");
+    printf("Spazi:\t ");
+    printf("\n");
+    printf("Numeri:\t\t\t");
+    for (i = 16; i<=25; i++){
+        printf("%c ",i+32);
+    }
+    printf("\n");
+    printf("Segni di punteggiatura:\t");
+    for (i = 1; i<=31; i++){
+        if (i == 1 || i == 2 || (i>=7 && i<=9) || (i>=12 && i<=15) || i == 26 || i == 27 || i == 31){
+            printf("%c ",i+32);
+        }
+        
+    }
+    printf("\n");
+    printf("Caratteri speciali:\t");
+    for (i = 3; i<=94; i++){
+        if ((i>=3 && i<=6) || i == 10 || i == 11 || (i>=28 && i<=30) || i == 32 || (i>=59 && i<=64) || (i>=91 && i<=94)){
+            printf("%c ",i+32);
+        }
+    }
+    printf("\n");
+}
+
 
 ///src/Analyzer/P.c
 //return file length in terms of chars
