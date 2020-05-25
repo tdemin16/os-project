3972d50589d33ab9fd10386f858120d4148d8ef3
Luigi Pusiol <luigi.pusiol@studenti.unitn.it>
2020-05-25 17:18:08 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,7 @@ $MAKE
 
 if [ $? -eq 0 ]; then 
     cd $BUILD_DIR
-<<<<<<< HEAD
     ./M ../src/
-=======
-    ./M ../src
->>>>>>> b2512110a82d58244b942ab4d8f90ba94a397450
 else
     echo Fail during building.
 fi
